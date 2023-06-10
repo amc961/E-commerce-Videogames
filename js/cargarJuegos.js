78e2ec250435afb940809f4e1ff6557fabcea4b0
Damian Lobos <damianlobos24@gmail.com>
2023-06-10 18:17:03 -0300

    Agregado estado de destacado a los juegos. Si uno es verdadero, su imagen se vera en el index

--- js/cargarJuegos.js
+++ js/cargarJuegos.js
@@ -4,7 +4,8 @@ const juegos = [
       nombre: "Devil May Cry 5",
       descripcion: "Devil May Cry 5 es un juego de acción hack and slash ambientado en la clásica franquicia de Capcom que, manteniendo a Dante como protagonista, introduce nuevos enemigos, jugabilidad y gráficos pero respetando los estándares a los que estamos acostumbrados en la franquicia DMC.",
       imagen: "https://i.blogs.es/94d4c1/devil-may-cry-5-analisis-01/1366_2000.jpg",
-      genero: "Hack and Slash"
+      genero: "Hack and Slash",
+      esDestacado: true
     },
     
     {
@@ -12,7 +13,8 @@ const juegos = [
       nombre: "Grand Theft Auto 5",
       descripcion: "Grand Theft Auto V es un videojuego de acción-aventura de mundo abierto en tercera persona desarrollado por el estudio escocés Rockstar North y distribuido por Rockstar Games.",
       imagen: "https://i.blogs.es/dfbccc/trucosgtavps4/1366_2000.jpg",
-      genero: "Accion - Aventura - Mundo Abierto"
+      genero: "Accion - Aventura - Mundo Abierto",
+      esDestacado: false
     },
   
     {
@@ -20,7 +22,8 @@ const juegos = [
       nombre: "Tales of Arise",
       descripcion: "Tales of Arise es un videojuego de rol de acción desarrollado y publicado por Bandai Namco Entertainment. Es una nueva entrega de la saga Tales of y su lanzamiento se produjo el 10 de septiembre de 2021 en Microsoft Windows, PlayStation 4 y 5 y Xbox One y Series X|S.",
       imagen: "https://static.bandainamcoent.eu/high/tales-of/tales-of-arise/03-news/tales-of-arise-release-day.jpg",
-      genero: "Rol - Aventura"
+      genero: "Rol - Aventura",
+      esDestacado: false
     },
   
     {
@@ -28,7 +31,8 @@ const juegos = [
       nombre: "Resident Evil 4 Remake",
       descripcion: "God of War es un videojuego de acción-aventura desarrollado por SCE Santa Monica Studio y publicado por Sony Interactive Entertainment. Su lanzamiento se produjo el 20 de abril de 2018 como un título exclusivo para la consola PlayStation 4.​ Posteriormente, fue lanzado en Microsoft Windows el 14 de enero de 2022.​",
       imagen: "https://img.youtube.com/vi/7RPSBeRIP6Y/maxresdefault.jpg",
-      genero: "Horror"
+      genero: "Horror",
+      esDestacado: false
     },
   
     {
@@ -36,14 +40,30 @@ const juegos = [
       nombre: "God of War (2018)",
       descripcion: "Tales of Arise es un videojuego de rol de acción desarrollado y publicado por Bandai Namco Entertainment. Es una nueva entrega de la saga Tales of y su lanzamiento se produjo el 10 de septiembre de 2021 en Microsoft Windows, PlayStation 4 y 5 y Xbox One y Series X|S.",
       imagen: "https://image.api.playstation.com/vulcan/img/rnd/202010/2217/ax0V5TYMax06mLzmkWeQMiwH.jpg",
-      genero: "Rol - Aventura - Accion - Hack and Slash"
+      genero: "Rol - Aventura - Accion - Hack and Slash",
+      esDestacado: false
     }
   ]
 
 const cargar = () =>{
-  if(localStorage.length == 0) {
-    localStorage.setItem("juegos", JSON.stringify(juegos))
-  }   
+ /*   if(localStorage.length == 0) {
+ 
+  } */
+  localStorage.setItem("juegos", JSON.stringify(juegos))
 }
 
-  cargar()
+cargar()
+
+const cargarDestacado = () =>{
+  let imgDestacada = document.getElementById("imgDestacada")
+  let juegos = JSON.parse(localStorage.getItem("juegos"))
+  let imagen =  ``
+  juegos.forEach((juego) => {
+    if(juego.esDestacado){
+      imagen = `<img src="${juego.imagen}" class="img-fluid" width="100%">`
+    }
+  });
+  imgDestacada.innerHTML = imagen
+}
+
+cargarDestacado()
