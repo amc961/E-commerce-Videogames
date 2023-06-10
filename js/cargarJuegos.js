2756f67726fb09c8de402fc824af4f70c7d34572
Damian Lobos <damianlobos24@gmail.com>
2023-06-10 19:23:04 -0300

    Agregada la imagen del producto destacado a la pagina de usuario logueado

--- js/cargarJuegos.js
+++ js/cargarJuegos.js
@@ -54,16 +54,3 @@ const cargar = () =>{
 
 cargar()
 
-const cargarDestacado = () =>{
-  let imgDestacada = document.getElementById("imgDestacada")
-  let juegos = JSON.parse(localStorage.getItem("juegos"))
-  let imagen =  ``
-  juegos.forEach((juego) => {
-    if(juego.esDestacado == "true"){
-      imagen = `<img src="${juego.imagen}" class="img-fluid" width="100%">`
-    }
-  });
-  imgDestacada.innerHTML = imagen
-}
-
-cargarDestacado()
