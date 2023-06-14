444d2603b8e6cecdb45fd801ecd788067f2aa77b
Damian Lobos <damianlobos24@gmail.com>
2023-06-14 14:27:19 -0300

    solucionado problema de carrito

--- js/suma.js
+++
@@ -1,8 +0,0 @@
-const sumatoriaNumeros = () =>{
-    let sumatoria = 0
-    let totales = document.getElementsByClassName("totale")
-    let inputs = document.getElementsByClassName("inputNumber")
-    for(let i = 0; i < totales.length; i++){
-        sumatoria  = sumatoria + (totales[i].value * inputs[i].value)
-    }
-}
