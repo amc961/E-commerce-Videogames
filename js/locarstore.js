37e6778f5fe8aa0b786bd7f0a8170159bc690add
axelsameul <emi.duarte643@gmail.com>
2023-06-17 14:46:59 -0300

    modificacion en login.html y locastore.js

--- js/locarstore.js
+++ js/locarstore.js
@@ -1,105 +1,54 @@
-let inputUser = document.getElementById("inputUser")
-let inputPass = document.getElementById("pass")
-let inputRepeatPass = document.getElementById("repeatPass")
+let InputEmail = document.getElementById("InputEmail")
+let InputPassword = document.getElementById("InputPassword")
 
-
-
-let divErrUser = document.getElementById("msgErrUser")
 let divErrPass = document.getElementById("divErrPass")
-let divErrRepeatPass= document.getElementById("divErrRepeatPass")
-let inputCheckId = document.getElementById("checkId")
-let buttonRegister= document.getElementById("buttonRegister")
+let divErrUser = document.getElementById("msgError")
 
-let localStorageArray = JSON.parse(localStorage.getItem("users")) || []
-
-let arrayUsuarios = []
-
+let buttonRegister = document.getElementById("buttonRegister")
 
 divErrUser.classList = "d-none"
 divErrPass.classList = "d-none"
-divErrRepeatPass.classList = "d-none"
- 
-let idUser = localStorageArray.length === 0 ? 1 :
-localStorageArray[localStorageArray.length - 1].id +1
-
-
-
-let objecForm = {
-
-id: idUser,
-username : "",
-pass: "",
-repeatPass : "",
-check : false
+let objectForm = {
+    username : "",
+    pass: ""
 }
 
+const inputChange = (event)=>{
+    const {name,value} = event.target
+objectForm [name]= value
 
+switch (name) {
+    case "username":
+        divErrUser.classList = "d-none"
+        InputEmail.classList.remove("is-invalid")
+        break;
 
-const inputChange =( event) =>{
-const {name, value} = event.target
-
-
-if (name === "check") {
-    objecForm [name] = inputCheckId.checked
-}else{
-    objecForm[name]= value
-    switch (name) {
-        case "username":
-            divErrUser.classList = "d-none"
-            inputUser.classList.remove("is-invalid")
+        case "pass":
+            divErrPass.classList = "d-none"
+            InputPassword.classList.remove("is-invalid")
             break;
-            case "pass":
-                divErrPass.classList = "d-none"
-                inputPass.classList.remove("is-invalid")
-                break;
-                case "repeatPass":
-                    divErrRepeatPass.classList = "d-none"
-                    inputRepeatPass.classList.remove("is-invalid")
-                    break;
-                
-      
-    }
     
 }
 
 
 
-
-
-
-
 }
 
-const register = ()=>{
-    console.log(objecForm);
-const {username,pass, repeatPass }= objecForm
+const register = ()=> {
+const {username,pass} = objectForm
 
-if (username && pass && repeatPass) {
-    arrayUsuarios.push(objecForm)
-    localStorage.setItem ("users", JSON.stringify(arrayUsuarios))
-}
-
-if(!username && !pass && !repeatPass){
-    alert("esta vacio")
-}else if (!username){
+  
+    
+ if (!username) {
     divErrUser.classList = "d-block text-danger"
-    inputUser.classList.add("is-invalid")
+    InputEmail.classList.add("is-invalid")
 }else if (!pass) {
     divErrPass.classList = "d-block text-danger"
-    inputPass.classList.add("is-invalid")
-}else if (!repeatPass) {
-    divErrRepeatPass.classList = "d-block text-danger"
-    inputRepeatPass.classList.add("is-invalid")
-}
+    InputPassword.classList.add("is-invalid")
+ }}
 
 
 
-}
-
-
-
-inputUser.addEventListener("input", inputChange)
-inputPass.addEventListener("input", inputChange)
-inputRepeatPass.addEventListener("input", inputChange)
+InputEmail.addEventListener("input", inputChange)
+InputPassword.addEventListener("input", inputChange)
 buttonRegister.addEventListener("click", register)
-inputCheckId.addEventListener("click", inputChange)
