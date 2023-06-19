c2e4072fc6df2a489144de9518be6b3feb0a72ff
Damian Lobos <damianlobos24@gmail.com>
2023-06-19 16:18:19 -0300

    Proteccion de registro completa

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,8 +1,18 @@
-const validar = () =>{
-    let inputEmail = document.getElementById("exampleInputEmail1")
-    let inputPass = document.getElementById("exampleInputPassword1")
-    let divEmail = document.getElementById("emailHelp")
-    let divPass = document.getElementById("passHelp")
-    validarInputComun(inputEmail, divEmail)
-    validarPass(inputPass, divPass)
+const validarRegistro = () =>{
+    let nombre = document.getElementById("nameInput")
+    let apellido = document.getElementById("lastNameInput")
+    let email = document.getElementById("emailInput")
+    let passw = document.getElementById("passInput")
+    let nombreTexto = document.getElementById("nameText")
+    let apeTexto = document.getElementById("lastNameText")
+    let emailTexto = document.getElementById("emailText")
+    let passTexto = document.getElementById("passText")
+    let validName = validarText(nombre, nombreTexto) 
+    let validApe = validarText(apellido, apeTexto)
+    let validEmail = validarEmail(email, emailTexto)
+    let validPass = validarPass(passw, passTexto)
+    if(validName && validApe && validEmail && validPass){
+        alert("Genial")
+    }
 }
+
