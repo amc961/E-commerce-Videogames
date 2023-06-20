ba7a76f9adacc3018043ab4b7ded4bc867663fd3
Damian Lobos <damianlobos24@gmail.com>
2023-06-20 11:35:23 -0300

    Boton para borrar items del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -11,18 +11,21 @@ tr.innerHTML =
     
 `   
 <tr>
-    <td>${prod.nombre}</td>
+    <td class="text-center">${prod.nombre}</td>
     <td>
-    <input type='number' class='w-50 inputNumber' name='cantidad' id='${prod.idJuego}'>
+    <input type='number' class='w-100 inputNumber' name='cantidad' id='${prod.idJuego}'>
     </td>
-    <td>${prod.precio}</td>
-    <td id='${totaltotal}' class="totale" >0</td>
+    <td class="text-center">${prod.precio}</td>
+    <td class="text-center" id='${totaltotal}' class="totale">0</td>
+    <td><button type="button"
+      class="container close" id="${prod.idJuego}"
+      onclick="quitar()">X</button></td>
 </tr>
 `
 tBody.appendChild(tr)
 
 const input = document.getElementById(prod.idJuego)
-input.addEventListener('change', (ev) => changeinput(ev, prod.precio, totaltotal))
+input.addEventListener('input', (ev) => changeinput(ev, prod.precio, totaltotal))
 }
 )
 
@@ -62,3 +65,22 @@ const changeinput = (event, precio, totaltotal) => {
     resultado = sumatoria
     totalfinal.innerHTML = sumatoria
 }
+
+
+
+function quitar() {
+    const boton = event.target; // Obtener el botón que se ha hecho clic
+    const idJuegoo = boton.id; // Obtener el idJuego del botón
+  
+    const fila = boton.parentNode.parentNode;
+    fila.remove();
+  
+    const CarritoLS = JSON.parse(localStorage.getItem('carrito')); // Obtener el carrito del almacenamiento local
+  
+    const CarritoActualizado = CarritoLS.filter((game) => game.idJuego.toString() !== idJuegoo.toString());
+  
+    console.log(CarritoActualizado);
+    
+    localStorage.setItem('carrito', JSON.stringify(CarritoActualizado));
+  }
+  
