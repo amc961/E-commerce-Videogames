6e1d94715ce655c2ed2615f9f70da89f8d94e929
GonzaloMainardi <gonzalomainardi@gmail.com>
2023-06-21 00:28:57 -0300

    se agrega proteccion a rutas para usuarios no logueados

--- js/privateruteAdmin.js
+++ js/privateruteAdmin.js
@@ -1,10 +1,14 @@
 let userss = JSON.parse(localStorage.getItem('loggeduser')); // Obtener los usuarios del localStorage y convertirlos a objeto
 
 function verificacionadmin(){
-if (userss[0].esAdmin === false) {
-  location.href = '../Pages/usuarioLogueado.html'; // Redirigir al usuario no administrador
+  if(userss[0]){
+    if (!userss[0].esAdmin) {
+      location.href = '../Pages/usuarioLogueado.html'; // Redirigir al usuario no administrador
           }
+        } else {
+          location.href = '../Pages/login.html'; // Redirigir al usuario no Logueado        
         }
+      }
 function cerrarsesion(){
   localStorage.removeItem("loggeduser")
 }
